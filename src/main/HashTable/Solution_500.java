f185b36a9595dcf5d431f04df49c57bd2c026824
wechanhe <1470325185@qq.com>
2018-11-15 13:36:13 +0800

    add solution 500

---
+++ src/main/HashTable/Solution_500.java
@@ -0,0 +1,13 @@
+package main.HashTable;
+
+/**
+ * question:
+ * solution:
+ * discussion:
+ */
+public class Solution_500 {
+    public String[] findWords(String[] words) {
+        String[] res = null;
+        return res;
+    }
+}
