4611f98d8e1267c43659047cd6edd2594f9475bf
wechanhe <1470325185@qq.com>
2018-11-17 00:36:21 +0800

    add solution 550

--- src/main/HashTable/Solution_500.java
+++ src/main/HashTable/Solution_500.java
@@ -2,12 +2,13 @@ package main.HashTable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * question:判断一个单词能否在键盘的一行敲完
  * solution: Map-key:单词，value:行
- * discussion:
+ * discussion: 首先确定在哪一行检查，String：toLowerCase，indexOf接口的使用
  */
 public class Solution_500 {
     public String[] findWords(String[] words) {
@@ -40,5 +41,26 @@ public class Solution_500 {
         for (int i=0;i<a_list.size();i++)
             res[i] = a_list.get(i);
         return res;
+    }
+    public String[] discuss(String[] words){
+        String[] letters = new String[]{"qwertyuiop","asdfghjkl","zxcvbnm"};
+        List<String> ans = new ArrayList<>();
+        for (String word : words){
+            boolean pass = true;
+            int row = 0;
+            if (letters[1].indexOf(word.toLowerCase().charAt(0)) >= 0)
+                row = 1;
+            else if (letters[2].indexOf(word.toLowerCase().charAt(0)) >= 0)
+                row = 2;
+            for (char c : word.toLowerCase().toCharArray()){
+                if (letters[row].indexOf(c) == -1){
+                    pass = false;
+                    break;
+                }
+            }
+            if (pass)
+                ans.add(word);
+        }
+        return ans.toArray(new String[ans.size()]);
     }
 }
