45b12c733e6e1772b43158810fcaede0d6110e80
wechanhe <1470325185@qq.com>
2018-11-17 00:21:27 +0800

    add solution 550 test

--- src/test/HashTable/HashTableTest.java
+++ src/test/HashTable/HashTableTest.java
@@ -1,7 +1,16 @@
 package HashTable;
 
-public class HashTableTest {
-    public void test_500(){
+import main.HashTable.Solution_500;
+import org.junit.Test;
 
+public class HashTableTest {
+    @Test
+    public void test_500(){
+        Solution_500 s = new Solution_500();
+        String[] words = {"Hello","Alaska","Dad","Peace"};
+        String[] res = s.findWords(words);
+        for (String word : res){
+            System.out.println(word);
+        }
     }
 }
