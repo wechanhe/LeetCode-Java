d1a87ab5d61e2d58b2273ef449b85e007ae5b83b
wechanhe <1470325185@qq.com>
2018-11-25 20:52:00 +0800

    solve some problems

---
+++ src/main/TwoPointer/Solution_344.java
@@ -0,0 +1,20 @@
+package main.TwoPointer;
+
+/**
+ * question:
+ * solution:
+ * discussion:
+ */
+public class Solution_344 {
+    public String reverseString(String s) {
+        int i = 0, j = s.length()-1;
+        char[] chars = s.toCharArray();
+        while (i < s.length()/2){
+            char tmp = chars[i];
+            chars[i] = chars[s.length()-i-1];
+            chars[s.length()-i-1] = tmp;
+            i++;
+        }
+        return new String(chars);
+    }
+}
